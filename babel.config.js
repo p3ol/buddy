18ae97fea3ac6cd94ceac3ad44f85c0086be9852
Ugo Stephant <ugo@poool.fr>
2019-10-28 21:22:46 +0100

    chore: init project

---
+++ babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+  ],
+  plugins: [
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-proposal-export-default-from"
+  ],
+};
---
+++ rollup.config.js
@@ -0,0 +1,69 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { eslint } from 'rollup-plugin-eslint';
+import { terser } from 'rollup-plugin-terser';
+
+const defaultConfig = () => ({
+  input: 'src/index.js',
+  output: {
+    name: 'buddy',
+    sourcemap: true,
+  },
+  plugins: [
+    eslint(),
+    babel({
+      exclude: 'node_modules/**',
+      externalHelpers: true,
+    }),
+  ],
+});
+
+const defaultUMDConfig = (minified = false, config = defaultConfig()) => ({
+  ...config,
+  output: {
+    ...config.output,
+    format: 'umd',
+  },
+  plugins: [
+    ...config.plugins,
+    resolve(),
+    commonjs(),
+    minified ? terser() : null,
+  ],
+});
+
+/*
+  COMMONJS / MODULE CONFIG
+*/
+const libConfig = (config = defaultConfig()) => ({
+  ...config,
+  output: [
+    { file: 'dist/buddy.cjs.js', format: 'cjs' },
+  ],
+});
+
+/*
+  UMD CONFIG
+*/
+const umdConfig = (config = defaultUMDConfig()) => ({
+  ...config,
+  output: {
+    ...config.output,
+    file: 'dist/buddy.js',
+  },
+});
+
+const umdMinConfig = (config = defaultUMDConfig(true)) => ({
+  ...config,
+  output: {
+    ...config.output,
+    file: 'dist/buddy.min.js',
+  },
+});
+
+export default [
+  libConfig(),
+  umdConfig(),
+  umdMinConfig(),
+];
