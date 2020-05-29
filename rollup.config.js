67afa96fa26715db38f046f56fb130d1945c2475
Ugo Stephant <ugo@poool.fr>
2020-05-29 21:14:29 +0200

    chore: preserve esmodules for better tree shaking

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
@@ -6,7 +7,10 @@ import { terser } from 'rollup-plugin-terser';
 
 const isForIE = process.env.BABEL_ENV === 'ie';
 const input = './src/index.js';
-const output = `./dist${isForIE ? '/ie' : ''}/buddy`;
+const output = `./dist${isForIE ? '/ie' : ''}`;
+const name = 'buddy';
+const formats = ['umd', 'cjs', 'esm'];
+
 const defaultExternals = [];
 const defaultGlobals = {};
 
@@ -18,52 +22,32 @@ const defaultPlugins = [
   }),
   resolve(),
   commonjs(),
+  terser(),
 ];
 
-export default [
-  // umd
-  {
-    input,
-    plugins: [
-      ...defaultPlugins,
-      terser(),
-    ],
-    external: defaultExternals,
-    output: {
-      file: `${output}.min.js`,
-      format: 'umd',
-      name: 'buddy',
-      sourcemap: true,
-      globals: defaultGlobals,
-    },
+export default formats.map(f => ({
+  input,
+  plugins: [
+    ...defaultPlugins,
+  ],
+  external: defaultExternals,
+  output: {
+    ...(f === 'esm' ? {
+      dir: `${output}/esm`,
+      chunkFileNames: '[name].js',
+    } : {
+      file: `${output}/${name}.${f}.js`,
+    }),
+    format: f,
+    name,
+    sourcemap: true,
+    globals: defaultGlobals,
   },
-
-  // cjs
-  {
-    input,
-    plugins: [
-      ...defaultPlugins,
-      terser(),
-    ],
-    external: defaultExternals,
-    output: {
-      file: `${output}.cjs.js`,
-      format: 'cjs',
-      sourcemap: true,
+  ...(f === 'esm' ? {
+    manualChunks: id => {
+      return id.includes('node_modules')
+        ? 'vendor'
+        : path.parse(id).name;
     },
-  },
-
-  // esm
-  {
-    input,
-    plugins: [
-      ...defaultPlugins,
-    ],
-    external: defaultExternals,
-    output: {
-      file: `${output}.esm.js`,
-      format: 'esm',
-      sourcemap: true,
-    },
-  },
-];
+  } : {}),
+}));
