c72ee4b5d0c54e406a40570a8a608e7c53c9a3a8
dackmin <ugo@poool.fr>
2021-06-03 11:39:01 +0200

    chore: update dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   clearMocks: true,
-  // collectCoverage: true,
-  // coverageDirectory: 'coverage',
-  // coveragePathIgnorePatterns: [
-  //   '/node_modules/',
-  //   'index.js',
-  // ],
-  modulePathIgnorePatterns: [
-    'old',
-  ],
   transform: {
     '^.+\\.js$': 'babel-jest',
   },
